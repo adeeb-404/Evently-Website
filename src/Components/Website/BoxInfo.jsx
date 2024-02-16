7495dc1354c1594e1f34414ac8d7194e5129b273
manojhebbar2k3 <manojhebbar2k3@gmail.com>
2024-02-16 19:04:47 +0530

    Added react icons

--- src/Components/Website/BoxInfo.jsx
+++ src/Components/Website/BoxInfo.jsx
@@ -1,5 +1,17 @@
 import { FaWandMagicSparkles } from "react-icons/fa6";
 import { AiFillAlert } from "react-icons/ai";
+import { BiSolidBellRing } from "react-icons/bi";
+import { MdManageAccounts } from "react-icons/md";
+import { AiOutlineAudit } from "react-icons/ai";
+import { BsClockFill } from "react-icons/bs";
+import { FaMobileAlt } from "react-icons/fa";
+import { LuHeartHandshake } from "react-icons/lu";
+import { TbBulbFilled } from "react-icons/tb";
+import { VscOrganization } from "react-icons/vsc";
+import { SiAwsorganizations } from "react-icons/si";
+import { PiHandTapFill } from "react-icons/pi";
+import { IoPerson } from "react-icons/io5";
+import { SiSymantec } from "react-icons/si";
 
 
 const Info = {
@@ -17,13 +29,13 @@ const Info = {
         " Implement a one-click RSVP button for attendees to quickly confirm their participation, streamlining the event registration process and encouraging higher attendance",
     },
     {
-      image: <AiFillAlert />,
+      image: <BiSolidBellRing />,
       box_heading: "Upcoming Event Alerts:",
       box_data:
         " Stay informed with timely event notifications as your upcoming gatherings approach, ensuring you never miss a moment of your planned experiences.",
     },
     {
-      image: <AiFillAlert />,
+      image: <MdManageAccounts />,
       box_heading: "Comprehensive Event Management:",
       box_data:
         "Effortlessly manage your event with centralized control—guest lists, schedules, and venue details—all in one place, ensuring seamless coordination.",
@@ -31,25 +43,25 @@ const Info = {
   ],
   "Why Evently?": [
     {
-      image: <AiFillAlert />,
+      image: <SiAwsorganizations />,
       box_heading: "Event Organization:",
       box_data:
         "Simplify the process of planning events effortlessly, providing an intuitive platform for customization, seamless organization, and stress-free event coordination.",
     },
     {
-      image: <AiFillAlert />,
+      image: <AiOutlineAudit />,
       box_heading: "On-the-Go Accessibility:",
       box_data:
         " Access and manage events conveniently while on the move, utilizing mobile app compatibility to receive real-time updates and information directly from your mobile device.",
     },
     {
-      image: <AiFillAlert />,
+      image: <BsClockFill />,
       box_heading: "Timely Event Updates: ",
       box_data:
         "Ensuring swift notifications for precise details on changes or additions to upcoming gatherings, guaranteeing you stay well-informed about event updates.",
     },
     {
-      image: <AiFillAlert />,
+      image: <FaMobileAlt />,
       box_heading: "Mobile Accessibility:",
       box_data:
       "Access and manage events on the go with mobile app compatibility, ensuring convenience and real-time updates directly from your smartphone.",
@@ -57,25 +69,25 @@ const Info = {
   ],
   "What we Promise": [
     {
-      image: <AiFillAlert />,
+      image: <LuHeartHandshake />,
       box_heading: "Complete Event Support:",
       box_data:
         "We promise to help you with everything for your event, like guests, schedules, and venue details, making planning easy.",
     },
     {
-      image: <AiFillAlert />,
+      image: <TbBulbFilled />,
       box_heading: "Easy Planning:",
       box_data:
         "Trust us for simple event planning, where we take care of all the details, making coordination effortless.",
     },
     {
-      image: <AiFillAlert />,
+      image: <PiHandTapFill />,
       box_heading: "One-Stop Control:",
       box_data:
         "Manage everything in one place with our platform, from guests to schedules, giving you control over your event.",
     },
     {
-      image: <AiFillAlert />,
+      image: <SiSymantec />,
       box_heading: "Flexible Event Experience:",
       box_data:
         "Expect a flexible event experience with us, whether it's online or in person, ensuring you can participate however you prefer.",
@@ -83,18 +95,18 @@ const Info = {
   ],
   "Developer's info": [
     {
-      image: <AiFillAlert />,
+      image: <IoPerson />,
       box_heading: "Manoj Hebbar",
       box_data:
-        "Front-End Developer : Crafting the user interface with a keen eye for design and user experience .",
+        "Front-End Developer : Crafting the user interface focusing on design and user experience .",
     },
     {
-      image: <AiFillAlert />,
+      image: <IoPerson />,
       box_heading: "Akshay Raj",
       box_data:`Back-End Developer : 
 Managing server-side logic, databases, and overall backend functionality.`    },
     {
-      image: <AiFillAlert />,
+      image: <IoPerson />,
       box_heading: "Mohammed Adeeb",
       box_data:
         " Full-Stack Developer : Bridging the gap between frontend and backend development.",
