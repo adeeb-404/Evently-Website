3250d6e727166d572c2ea0facce7a77b26a7b383
manojhebbar2k3 <manojhebbar2k3@gmail.com>
2024-02-16 16:50:10 +0530

    Made changes in content

--- src/Components/Website/BoxSection.jsx
+++ src/Components/Website/BoxSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BoxSectionBox from "./BoxSectionBox";
+import Info from "./BoxInfo";
 
 const BoxSection = ({ heading }) => {
   return (
@@ -8,7 +9,7 @@ const BoxSection = ({ heading }) => {
       <div className=" m-[1rem] my-20">
         <h1 className="_flex md:flex-row text-[3rem] font-mono">{heading}</h1>
         <div className="_flex md:flex-row my-8 w-full flex-wrap">
-          {[...new Array(4)].map((el, i) => (
+          {[...new Array(Info[heading].length)].map((el, i) => (
             <BoxSectionBox key={i} id={i} heading={heading} />
           ))}
         </div>
