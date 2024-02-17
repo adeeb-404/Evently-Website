549cb90cf3cbb0db780b772ca4354d220a72520a
Adeeb <adeebm195@gmail.com>
2024-02-17 15:24:29 +0530

    add Log out button

--- src/Components/MainApp/JSX-components/homeNav.jsx
+++ src/Components/MainApp/JSX-components/homeNav.jsx
@@ -2,25 +2,19 @@ import React, { useEffect } from "react";
 import "../CSS-components/HomeNav.css";
 import NavLogos from "./navLogos";
 import SearchBar from "./SearchBar";
-import { useNavigate } from "react-router-dom";
 
 export default function HomeNav(props) {
-  const navigator = useNavigate();
-
-  async function handleLogOut() {
-    localStorage.removeItem("username");
-    localStorage.removeItem("fname");
-    localStorage.removeItem("lname");
-    navigator("/");
-  }
-
   function handleSearch(value) {
     console.log(value);
     // console.log(isSearch);
     props.handleSearch(value);
   }
   return (
-    <div className="navContainer bg-gradient-to-b from-slate-950 to-slate-900 text-slate-200">
+    <div
+      className="navContainer
+    
+     bg-gradient-to-b from-slate-950 to-slate-900 text-slate-200  "
+    >
       <div>
         <img src="Logo.png" className=" h-10 md:h-20 lg:h-[3.5rem] mx-5"></img>
         <h1 className="nav--evently text-xl hidden lg:flex text-md lg:text-2xl mr-12 font-heading_font">
@@ -28,7 +22,7 @@ export default function HomeNav(props) {
         </h1>
       </div>
       <SearchBar handleSearch={handleSearch} />
-      <h1 onClick={handleLogOut}>Log out</h1>
+
       <NavLogos />
     </div>
   );
--- src/Components/MainApp/JSX-components/navLogos.jsx
+++ src/Components/MainApp/JSX-components/navLogos.jsx
@@ -5,8 +5,11 @@ import { CgProfile } from "react-icons/cg";
 import { useNavigate } from "react-router-dom";
 
 const NavLogos = () => {
+  const navigator = useNavigate();
   const [name, setname] = useState("");
+  const [show, setshow] = useState(false);
   const navigate = useNavigate();
+
   useEffect(() => {
     let name = localStorage.getItem("username");
     if (!name) {
@@ -16,16 +19,30 @@ const NavLogos = () => {
     setname(name);
   }, []);
   // setname(localStorage.getItem("username"));
+  async function handleLogOut() {
+    localStorage.removeItem("username");
+    localStorage.removeItem("fname");
+    localStorage.removeItem("lname");
+    navigator("/");
+  }
   return (
     <>
       <div>
-        <FaBell
+        {/* <FaBell
           className="reactIcon hover:shadow-slate-300 shadow-xl"
           id="settingsIcon"
-        />
-        <a href="https://www.youtube.com/">
-          <IoMdSettings className="reactIcon hover:shadow-slate-300 shadow-xl" />
-        </a>
+        /> */}
+        <div className="relative pb-1" onClick={() => setshow(!show)}>
+          <IoMdSettings className="reactIcon    hover:shadow-slate-300 shadow-xl " />
+        </div>
+        {show && (
+          <button
+            onClick={handleLogOut}
+            className=" absolute -bottom-12  z-1 bg-slate-900 p-3 hover:bg-black hover:shadow-slate-300 hover:shadow-lg border-2"
+          >
+            Log out
+          </button>
+        )}
         <h2>Welcome {name}</h2>
         <CgProfile
           className="reactIcon hover:shadow-slate-300 shadow-xl"
